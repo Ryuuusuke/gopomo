611a4ff681102b6b972278904f90016de5178020
Ryuuusuke <makewishmr@gmail.com>
2025-12-27 12:45:37 +0700

    refactor to make it efficient

--- src/render.go
+++ src/render.go
@@ -2,6 +2,21 @@ package src
 
 import "strings"
 
+func (c Character) RenderRow(row int, color Color) string {
+	var segment Segment
+
+	switch c.kind {
+	case CharNumber:
+		segment = numberSegments[c.value*5+row]
+	case CharColon:
+		segment = colonSegments[row]
+	default:
+		segment = SegmentEmpty
+	}
+
+	return segment.Render(color)
+}
+
 func renderClockTime(timeText string, color Color) string {
 	chars := parseTimeToCharacters(timeText)
 	lines := make([]string, 5)
--- src/segment.go
+++ src/segment.go
@@ -10,3 +10,26 @@ const (
 	SegmentSides
 	SegmentEmpty
 )
+
+// 6 SPACES TOTAL
+func (s Segment) Render(color Color) string {
+	bg := color.Background()
+	reset := ColorReset
+
+	switch s {
+	case SegmentFull:
+		return bg + "      " + reset
+	case SegmentLeft:
+		return bg + "  " + reset + "    "
+	case SegmentCenter:
+		return "  " + bg + "  " + reset + "  "
+	case SegmentRight:
+		return "    " + bg + "  " + reset
+	case SegmentSides:
+		return bg + "  " + reset + "  " + bg + "  " + reset
+	case SegmentEmpty:
+		return "      "
+	default:
+		return "      "
+	}
+}
